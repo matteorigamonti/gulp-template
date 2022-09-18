7f804b3a5a91b90e60547efe12de4601f785a940
Matteo Rigamonti <matteo_rigamonti@hotmail.it>
2022-09-18 23:26:35 +0200

    add gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,39 @@
 const gulp = require("gulp"); // https://gulpjs.com/
 const nunjucksRender = require("gulp-nunjucks-render"); // https://www.npmjs.com/package/gulp-nunjucks-render
 const htmlmin = require("gulp-htmlmin"); // https://github.com/jonschlinkert/gulp-htmlmin
+const sass = require("gulp-sass")(require("sass")); // https://www.npmjs.com/package/gulp-sass
+const sourcemaps = require("gulp-sourcemaps"); // https://github.com/gulp-sourcemaps/gulp-sourcemaps
+const imagemin = require("gulp-imagemin"); // https://www.npmjs.com/package/gulp-imagemin
+const webp = require("gulp-webp"); // https://www.npmjs.com/package/gulp-webp
 
-function render() {
+function htmlMin() {
 	return gulp
-		.src("src/*.html")
+		.src("./app/*.html")
 		.pipe(
 			nunjucksRender({
-				path: ["src/"], // String or Array
+				path: ["app/"],
 			})
 		)
 		.pipe(htmlmin({ collapseWhitespace: true }))
 		.pipe(gulp.dest("dist"));
 }
 
-exports.render = render;
+function scss() {
+	return gulp.src("./app/scss/**/*.scss").pipe(sourcemaps.init()).pipe(sass().on("error", sass.logError)).pipe(sourcemaps.write()).pipe(gulp.dest("./dist/css"));
+}
+
+function image() {
+	return gulp
+		.src("./app/img/**/*")
+		.pipe(imagemin([imagemin.mozjpeg({ quality: 80, progressive: true })]))
+		.pipe(webp())
+		.pipe(gulp.dest("./dist/img"));
+}
+
+exports.scss = scss;
+exports.htmlMin = htmlMin;
+exports.image = image;
+
+exports.watch = function () {
+	gulp.watch("./app/scss/**/*.scss", scss);
+};
